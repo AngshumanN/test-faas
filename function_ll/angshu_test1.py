fdbfbf26cd76f1d2db94de2be76429f8b3eae475
AngshumanN <angshumann@exponentialai.com>
2020-03-31 20:47:25 +0530

    jj

--- function_ll/angshu_test1.py
+++ function_ll/angshu_test1.py
@@ -1,6 +1,6 @@
 from xpms_helper.model import domain_util
 
 def angshu_test1(**kwargs):
-    json= domain_util.get_domain_object({"solution_id": "daglib", "doc_id": "66ae918d-c45a-463f-beed-8b6415289846", "root_id": "66ae918d-c45a-463f-beed-8b6415289846"})
+    json= domain_util.get_domain_object({"solution_id": "libdag", "doc_id": "d79c768b-7282-4e64-a14f-625205d4c5d3", "root_id": "d79c768b-7282-4e64-a14f-625205d4c5d3"})
     print(json)
 
