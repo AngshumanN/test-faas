331622a09291121b717b2e518e3694b6ed006088
AngshumanN <angshumann@exponentialai.com>
2020-03-31 18:36:59 +0530

    gg

--- function_ll/angshu_test1.py
+++ function_ll/angshu_test1.py
@@ -1,6 +1,6 @@
 from xpms_helper.model import domain_util
 
-def angshu_test():
+def angshu_test_1(param):
     json= domain_util.get_domain_object({"solution_id": "daglib", "doc_id": "66ae918d-c45a-463f-beed-8b6415289846", "root_id": "66ae918d-c45a-463f-beed-8b6415289846"})
     print(json)
 
