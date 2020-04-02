fc8e9ce064e596c43f2a036106bd57b88ebd95dc
AngshumanN <angshumann@exponentialai.com>
2020-04-03 01:36:21 +0530

    updated angshu_test

--- function_ll/angshu_test1.py
+++
@@ -1,5 +0,0 @@
-# from xpms_helper.model import domain_util
-
-def angshu_test1(config,**kwargs):
-    # json= domain_util.get_domain_object({"solution_id": "demofunction", "doc_id": "8be5a4d7-73a4-46a8-9568-7ce4b5ec22d3", "root_id": "8be5a4d7-73a4-46a8-9568-7ce4b5ec22d3"})
-    return {"config": config, "kwargs": kwargs}
